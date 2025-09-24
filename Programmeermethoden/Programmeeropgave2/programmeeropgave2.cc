d8e3eb87abf9bff9f22c40139ba4608a5c182346
Repsajsov <jaspervos12@outlook.com>
2025-09-24 17:39:25 +0200

    wat een chaos...

--- Programmeermethoden/Programmeeropgave2/programmeeropgave2.cc
+++ Programmeermethoden/Programmeeropgave2/programmeeropgave2.cc
@@ -25,38 +25,62 @@ void codeer(string &invoerFile, string &uitvoerFile, int pincode)
     ifstream invoer(invoerFile, ios::in);
     ofstream uitvoer(uitvoerFile, ios::out);
 
-    char karakter, vorigKarakter;
+    char karakter, vorigKarakter, karakterUitvoer;
     int caesar;
-    int getal;
+    int getal = 0;
     int exponent;
     int count = 0;
+    bool veranderPincode = false;
     while (!invoer.eof())
     {
         vorigKarakter = karakter;
         karakter = invoer.get();
         if (karakter == '\n')
         {
+            uitvoer.put(karakter);
             count = -1;
         }
-        else if (karakter != '\r' && karakter != '\t')
+        else if (karakter == '\t' || karakter == 'r')
         {
-            if (isCijfer(karakter))
-            {
-                if (isCijfer(vorigKarakter))
-                {
-                    getal = (getal * 10) + (karakter - '0');
-                }
-                else
-                {
-                    getal = karakter - '0';
-                }
-            }
+            uitvoer.put(karakter);
+        }
+        else
+        {
             exponent = 3 - (count % 4);
             caesar = pincode / power(10, exponent) % 10;
-            karakter = ((karakter - 32 + caesar) % 95) + 32;
+            karakterUitvoer = ((karakter - 32 + caesar) % 95) + 32;
+            uitvoer.put(karakterUitvoer);
         }
-        uitvoer.put(karakter);
+        if (isCijfer(karakter))
+        {
+            if (isCijfer(vorigKarakter))
+            {
+                getal = (getal * 10) + (karakter - '0');
+            }
+            else
+            {
+                getal = karakter - '0';
+            }
+        }
+        else
+        {
+            if (isCijfer(vorigKarakter))
+            {
+                if (getal >= 0 && getal <= 10000)
+                {
+                    veranderPincode = true;
+                }
+                cout << getal << endl;
+            }
+        }
+
         count++;
+        if (veranderPincode)
+        {
+            pincode = getal;
+            getal = 0;
+            veranderPincode = false;
+        }
     }
     invoer.close();
     uitvoer.close();
